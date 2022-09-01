104359d243af19181ffeb08490b7849ef18884ef
FoongL <leung.foong@gmail.com>
2022-09-01 12:01:07 +0900

    fixed pg connection

--- models/index.js
+++ models/index.js
@@ -1,25 +1,34 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.js')[env];
+const env = process.env.NODE_ENV || "development";
+const config = require(__dirname + "/../config/config.js")[env];
 const db = {};
 
 let sequelize;
-if(process.env.DATABASE_URL){
-  sequelize= new Sequelize(process.env.DATABASE_URL,{
-    dialect: process.env.DB_DIALECT
-  })
-}
-else if (config.use_env_variable) {
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+      dialect: process.env.DB_DIALECT,
+    },
+  });
+} else if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
 }
-
 
 // default from sequlize init command
 
@@ -33,17 +42,16 @@ else if (config.use_env_variable) {
 //     db[model.name] = model;
 //   });
 
-
 // alternative way to do it (Rocket Gitbook way)
 
 // import our models
-const initUsersModel= require('./users')
-const initItemModel = require('./items')
-const initCategoriesModel = require('./categories');
+const initUsersModel = require("./users");
+const initItemModel = require("./items");
+const initCategoriesModel = require("./categories");
 
-db.users = initUsersModel(sequelize, Sequelize.DataTypes)
-db.items = initItemModel(sequelize, Sequelize.DataTypes)
-db.categories = initCategoriesModel(sequelize, Sequelize.DataTypes)
+db.users = initUsersModel(sequelize, Sequelize.DataTypes);
+db.items = initItemModel(sequelize, Sequelize.DataTypes);
+db.categories = initCategoriesModel(sequelize, Sequelize.DataTypes);
 
 // Associations:
 
@@ -51,17 +59,13 @@ db.categories = initCategoriesModel(sequelize, Sequelize.DataTypes)
 // db.users.hasMany(db.items)
 // db.items.belongsTo(db.users)
 
-
-
 // Many to Many
-db.items.belongsToMany(db.categories, {through: 'category-item'})
-db.categories.belongsToMany(db.items, {through: 'category-item'})
+db.items.belongsToMany(db.categories, { through: "category-item" });
+db.categories.belongsToMany(db.items, { through: "category-item" });
 
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Connected to SQL database');
-  })
+sequelize.authenticate().then(() => {
+  console.log("Connected to SQL database");
+});
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
